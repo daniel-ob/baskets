9f7fd21bc17372e116ff8505f25389c598154a44
daniel-ob <danobrey@gmail.com>
2023-09-20 20:39:25 +0200

    Missing translation on profile page

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import UpdateView
 
 from accounts.forms import CustomUserForm
@@ -12,7 +13,7 @@ class ProfileView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     form_class = CustomUserForm
     template_name = "account/profile.html"
     success_url = reverse_lazy("profile")
-    success_message = "Vos coordonnées ont été mises à jour avec succès."
+    success_message = _("Your details have been updated")
 
     def get_object(self):
         return self.request.user
